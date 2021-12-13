fd3281ba73db224973393820aaebbffb46398840
othonbreener <othonbrener123@gmail.com>
2021-12-13 08:42:45 -0300

    Criando uma view básica para retornar um html mínimo

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve
+from django.http import HttpRequest
+
 from lists.views import home_page
 
 # Create your tests here.
@@ -9,3 +11,13 @@ class HomePageTest(TestCase):
     def test_root_url_direciona_para_a_rota_home_page(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+    
+    
+    def test_home_page_retorna_o_html_correto(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf-8')
+
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
