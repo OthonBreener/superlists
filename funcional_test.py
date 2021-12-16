07689209ca1c4962c58496ba9f8d77ba95fcd44a
othonbreener <othonbrener123@gmail.com>
2021-12-16 08:52:17 -0300

    Teste funcional verifica se nós podemos entrar com um item de tarefa

--- funcional_test.py
+++ funcional_test.py
@@ -1,5 +1,8 @@
-from selenium import webdriver
 import unittest
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -21,16 +24,40 @@ class NewVisitorTest(unittest.TestCase):
         de tarefas (to-do)
         """
         self.assertIn('To-Do', self.browser.title)
+
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        
+        self.assertIn('To-Do', header_text)
         """
         Ela é convidada a inserir um item de tarefa imediatamente.
 
-        Ela digita 'Comprar penas de urubu' em uma caixa de texto.
+        """
+        inputbox = self.browser.find_element_by_id('id_new_item')
 
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Digite uma tarefa')
+
+        """
+
+        Ela digita 'Comprar penas de urubu' em uma caixa de texto.
+        """
+        inputbox.send_keys('Comprar penas de urubu')
+
+        """
         Quando ela tecla enter, a página é atualizada, e agora a página
         lista:
             1). Comprar penas de urubu
         como um item em uma lista de tarefas.
+        """
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1). Comprar penas de urubu' for row in rows)
+        )
+
+        """
         Ainda continua havendo uma caixa de texto convidando-a a acrescentar
         outro item. Ela insere: 'Rabo de Galinha'.
 
@@ -39,7 +66,7 @@ class NewVisitorTest(unittest.TestCase):
         Sheila se pergunta se o site lembrará de sua lista. Então ela nota que o site
         gerou um url único para ela. Ela acessa esse url e sua lista de tarefas continua lá.
         """
-        self.fail('Fim do Teste')
+        self.fail('Termine o Teste')
     
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
