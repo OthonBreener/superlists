dc7e496adbc22247ae48dd635bb0d354b832645f
othonbreener <othonbrener123@gmail.com>
2021-12-19 12:39:52 -0300

    Refatorando a rota home e usando o assert para templates do django

--- lists/tests.py
+++ lists/tests.py
@@ -1,23 +1,11 @@
 from django.test import TestCase
 from django.urls import resolve
-from django.http import HttpRequest
-
-from lists.views import home_page
 
 # Create your tests here.
 
 class HomePageTest(TestCase):
-
-    def test_root_url_direciona_para_a_rota_home_page(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-    
     
     def test_home_page_retorna_o_html_correto(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf-8')
-
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
